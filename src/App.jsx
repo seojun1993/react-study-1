15ab8855c7b1f5c6a0ab49e63b027ab45eb4abe0
이서준 <sj@iseojun-ui-MacBookAir.local>
2025-12-11 20:41:19 +0900

    feat : 가위바위보 로직 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Box from './component/Box.jsx';
+import { useState } from 'react';
 
 // 1. 박스 2개 (타이틀, 사진, 결과)
 // 2. 가위 바위 보 버튼이 있다.
@@ -7,17 +8,48 @@ import Box from './component/Box.jsx';
 // 4. 컴퓨터는 랜덤하게 아이템 선택이 된다.
 // 5. 3,4의 결과를 가지고 누가 이겼는지 승패를 따진다.
 // 6. 승패 결과에 따라 테두리 색이 바뀐다 ( win - 초록, lose - 빨강, draw - 검정)
+
+
+const choice = {
+  rock: {
+    name : 'Rock',
+    img : 'https://thumb.ac-illust.com/07/07b5ccdc4ec9d6acbc516d53ef1dab33_t.jpeg',
+  },
+  scissors: {
+    name : 'Scissors',
+    img : 'https://upload.wikimedia.org/wikipedia/commons/9/97/Standard_household_scissors.jpg',
+  },
+  paper: {
+    name : 'Paper',
+    img : 'https://e7.pngegg.com/pngimages/348/695/png-clipart-rock-paper-scissors-rock-paper-scissors-lizard-spock-rock-paper-scissors-llc-hands-hand-love-game.png',
+  }
+}
+
+
 function App() {
+  const [userSelect, setUserSelect] = useState(null);
+  const [computerSelect, setComputerSelect] = useState(null);
+
+  const play = ( userChoice ) => {
+    setUserSelect(choice[userChoice]);
+    computerPlay();
+  }
+
+  const computerPlay = () => {
+    const computerChoice = Object.keys(choice)[Math.floor(Math.random() * Object.keys(choice).length)];
+    setComputerSelect(choice[computerChoice]);
+  }
+
   return (
     <div class='container'>
       <div className='box-container'>
-        <Box title="You" />
-        <Box title="Computer" />
+        <Box title="You" item={userSelect}/>
+        <Box title="Computer" item={computerSelect}/>
       </div>
       <div className='button-container'>
-        <button>가위</button>
-        <button>바위</button>
-        <button>보</button>
+        <button onClick={() => play('scissors')}>가위</button>
+        <button onClick={() => play('rock')}>바위</button>
+        <button onClick={() => play('paper')}>보</button>
       </div>
     </div>
   )
